983e29153aa0308ac5cb2aa829136a04c52e98ed
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-01 12:39:52 -0300

    feat: create car service

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,1 +1,19 @@
-// import
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import GenericService, { ServiceError } from './GenericService';
+import CarModel from '../models/CarModel';
+
+class CarService extends GenericService<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+  
+  create = async (obj: Car): Promise<Car | ServiceError | null> => {
+    const { error } = CarSchema.validate(obj);
+    if (error) {
+      throw error;
+    }
+    return this.model.create(obj);
+  };
+}
+
+export default CarService;
