b9e977dd2e332f970514f650f0b44f9e2e24f13e
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-01 12:33:56 -0300

    feat: add a validation with joi on car interfaces

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,6 +1,17 @@
+import joi from 'joi';
 import { Vehicle } from './VehicleInterface';
 
 export interface Car extends Vehicle {
   doorsQty: number,
   seatsQty: number,
 }
+
+export const CarSchema = joi.object({
+  model: joi.string().min(3).required(),
+  year: joi.number().min(1900).max(2022).required(),
+  color: joi.string().min(3).required(),
+  status: joi.boolean(),
+  buyValue: joi.number().integer().required(),
+  doorsQty: joi.number().min(2).max(4).required(),
+  seatsQty: joi.number().min(2).max(7).required(),
+});
