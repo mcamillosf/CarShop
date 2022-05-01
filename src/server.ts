4e0acc97366f0fee9d9f5cda0f9dfcb3e8c88cf0
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-01 20:16:38 -0300

    create server.ts

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import CustomRouter from './routes/GenericRouter';
+import App from './app';
+
+import CarController from './controllers/CarController';
+
+import { Car } from './interfaces/CarInterface';
+
+const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
+
+export default server;
