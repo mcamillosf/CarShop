eed3ddc78065fc63455a892da94c5f8e02af4b18
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-01 14:56:37 -0300

    feat: create generic controller

---
+++ src/controllers/GenericController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express';
+import GenericService from '../services/GenericService';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+abstract class GenericController<T> {
+  abstract rout: string;
+
+  protected erros = ControllerErrors;
+
+  constructor(protected service: GenericService<T>) { }
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const objs = await this.service.read();
+      return res.json(objs);
+    } catch (error) {
+      return res.status(500).json({ error: this.erros.internal });
+    }
+  };
+
+  abstract readOne(
+    req: Request<{ id: string }>,
+    res: Response<T | ResponseError>
+  ): Promise<typeof res>;
+
+  abstract update(
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res>;
+
+  abstract delete(
+    req: Request<{ id: string }>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
+
+export default GenericController;
