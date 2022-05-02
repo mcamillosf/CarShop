0c0af996aa529bbf56e7482346636c21307fe837
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-01 23:09:31 -0300

    refactor: changing from joi to zod for schema validation

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,4 +1,4 @@
-import joi from 'joi';
+import { z } from 'zod';
 import { Vehicle } from './VehicleInterface';
 
 export interface Car extends Vehicle {
@@ -6,13 +6,40 @@ export interface Car extends Vehicle {
   seatsQty: number,
 }
 
-export const CarSchema = joi.object({
-  _id: joi.string(),
-  model: joi.string().min(3).required(),
-  year: joi.number().min(1900).max(2022).required(),
-  color: joi.string().min(3).required(),
-  status: joi.boolean(),
-  buyValue: joi.number().integer().required(),
-  doorsQty: joi.number().min(2).max(4).required(),
-  seatsQty: joi.number().min(2).max(7).required(),
+export const CarSchema = z.object({
+  _id: z.string(),
+  model: z.string({
+    required_error: 'model is required',
+    invalid_type_error: 'model must be a string',
+  }).min(3, { message: 'model must be 3 or more characters long' }),
+  year: z.number({
+    required_error: 'year is required',
+    invalid_type_error: 'year must be a number',
+  }).min(1900, { 
+    message: 'year must be equal or higher than 1900' }).max(2022, {
+    message: 'year must be equal or lower than 2022',
+  }),
+  color: z.string({
+    required_error: 'color is required',
+    invalid_type_error: 'color must be a string',
+  }).min(3, { message: 'color must be 3 or more characters long' }),
+  status: z.boolean(),
+  buyValue: z.number({
+    required_error: 'buyValue is required',
+    invalid_type_error: 'buyValue must be a number',
+  }).int(),
+  doorsQty: z.number({
+    required_error: 'doorsQty is required',
+    invalid_type_error: 'doorsQty must be a number',
+  }).min(2, { 
+    message: 'doorsQty must equal or higher than 2' }).max(4, {
+    message: 'doorsQty must be equal or lower than 4',
+  }),
+  seatsQty: z.number({
+    required_error: 'seatsQty is required',
+    invalid_type_error: 'seatsQty must be a number',
+  }).min(2, { 
+    message: 'seatsQty must be equal or higher than 2' }).max(7, {
+    message: 'seatsQty must be equal or lower than 7',
+  }),
 });
