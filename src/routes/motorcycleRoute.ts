596e394807754096f54765414fe8ac83dae6b878
mcamillosf <matheuscamillo@hotmail.com.br>
2022-05-11 22:47:27 -0300

    fix: fix lint error on motorcycle route

--- src/routes/motorcycleRoute.ts
+++ src/routes/motorcycleRoute.ts
@@ -1,7 +1,8 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 import { Router } from 'express';
 import GenericController from '../controllers/GenericController';
 import validate from '../middlewares/validateInput';
+
+const motorcycles = '/motorcycles/:id';
 
 class MotorCycleRouter<T> {
   public router: Router;
@@ -15,10 +16,10 @@ class MotorCycleRouter<T> {
     // route: string = controller.route,
   ) {
     this.router.get('/motorcycles', controller.read);
-    this.router.get('/motorcycles/:id', controller.readOne);
+    this.router.get(motorcycles, controller.readOne);
     this.router.post('/motorcycles', validate, controller.create);
-    this.router.put('/motorcycles/:id', controller.update);
-    this.router.delete('/motorcycles/:id', controller.delete);
+    this.router.put(motorcycles, controller.update);
+    this.router.delete(motorcycles, controller.delete);
   }
 }
 
